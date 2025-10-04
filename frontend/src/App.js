922f3571e7af5f182ada21d96705a96977810062
Narendra618 <vvny5555@gmail.com>
2025-10-04 12:30:08 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>My Diary App</h1>
+      <p>Welcome! Your React app is working 🎉</p>
+    </div>
+  );
+}
+
+export default App;
