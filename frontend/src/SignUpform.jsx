4af7c6547356a10df41d4d59a346f75058fd0604
Narendra618 <vvny5555@gmail.com>
2025-10-10 15:35:22 +0530

    added submit button

--- frontend/src/SignUpform.jsx
+++ frontend/src/SignUpform.jsx
@@ -2,13 +2,19 @@ import React from "react";
 function SignUpform() {
   return (
     <div className="forms">
-      <form className="form">
+      <form className="form" >
+        <h2><center>Sign Up</center></h2>
+        <label className="label">
+        Username:</label>
       <input type="text" className="input" placeholder="Enter Username"/>
+      <label className="label1">
+        Email:</label>
       <input type="email" className="input1" placeholder="Enter Email" />
+      <label className="label2">password:</label>
       <input type="password" className="input2" placeholder="Enter Password" />
+      <button type="submit" className="button">submit</button>
       </form>
     </div>
   );
 }
-
 export default SignUpform;
