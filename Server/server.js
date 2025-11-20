c6f496cd007072d7e80088e9d766ec967d54d85d
Narendra618 <vvny5555@gmail.com>
2025-11-20 18:59:14 +0530

    fix: environment variables

--- Server/server.js
+++ Server/server.js
@@ -23,9 +23,6 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use('/', userRoutes);
 
-console.log("DEBUG MONGO_URI:", process.env.MONGO_URI);
-
-
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
